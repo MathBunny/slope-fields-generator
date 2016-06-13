ffd10ddd3c2f6dcf4ff0dca962fa2bda4d94b3f0
Horatiu Lazu <horatiulazu@gmail.com>
2016-06-13 19:47:20 -0400

    Add working grid system

--- app/src/main/java/ca/horatiu/slope_field_generator/Graph.java
+++ app/src/main/java/ca/horatiu/slope_field_generator/Graph.java
@@ -1,108 +1,109 @@
 package ca.horatiu.slope_field_generator;
-
 import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Graph.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Graph#newInstance} factory method to
- * create an instance of this fragment.
+ * Created by Horatiu on 13/06/2016.
  */
-public class Graph extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class Graph extends View {
+    static final int MIN_PIXELS_PER_SQUARE = 100;
+    Paint paint;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    int startX = 0;
+    int endX = 500;
+    int startY = 0;
+    int endY = 100;
+    int height; //height
+    int width; //width
+    int startYGFX; //position to start executing Y in graphics
 
-    private OnFragmentInteractionListener mListener;
+    int skip;
 
-    public Graph() {
-        // Required empty public constructor
+    public Graph(Context context) {
+        super(context);
+        paint = new Paint();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Graph.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Graph newInstance(String param1, String param2) {
-        Graph fragment = new Graph();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void drawXAxisText(Canvas canvas){
+        final int yBump = 20;//10
+        final int xBump = 9;
+        paint.setColor(android.graphics.Color.BLACK);
+        paint.setTextSize(35);
+        int iter = 0;
+        for(int x = MIN_PIXELS_PER_SQUARE; x <= width; x+=MIN_PIXELS_PER_SQUARE, iter++){ //<= width-MIN_
+            String output = (startX + iter * skip) + ""; //start at x, then add accordingly :-)
+            canvas.drawText(output, x-((int)output.length())*xBump, height-yBump, paint);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graph, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    public void drawYAxisText(Canvas canvas){
+        final int yBump = 10;
+        final int xBump = 9;
+        paint.setColor(android.graphics.Color.BLACK);
+        paint.setTextSize(35); //start at the end
+        int iter = 0;
+        for(int y = startYGFX; y >= 0; y-=MIN_PIXELS_PER_SQUARE, iter++){ //<= width-MIN_
+            String output = (startY + iter * skip) + "";
+            canvas.drawText(output, xBump, y-yBump, paint);
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    //draws the line
+    public void drawGrid(Canvas canvas){
+        paint.setColor(Color.BLACK);
+
+        for(int x = 0; x <= width; x+= MIN_PIXELS_PER_SQUARE){
+            canvas.drawLine(x, 0,  x,  height, paint);
         }
+        for(int y = 0; y <= height; y+= MIN_PIXELS_PER_SQUARE){
+            for(int z = 0; z < 4; z++)
+                canvas.drawLine(0, y+z, width, y+z+1, paint);
+            startYGFX = y;
+        }
+        //canvas.save();
+        //Log.d("OK", "OK __ ADDED" +skip);
+    }
+
+    public void generateStep(){
+        height = getHeight()/2;
+        width = getWidth();
+        int stepHorizontal = ((endX-startX+2)*MIN_PIXELS_PER_SQUARE)/width+1; //bug: 0-500
+        int stepVertical = ((endY-startY+2)*MIN_PIXELS_PER_SQUARE)/height+1;
+        Log.d("skipGap", "Gap: " + stepHorizontal + " " + stepVertical);
+
+        skip = Math.max(stepHorizontal, stepVertical); //okay done. fix this in the future by centering.
+        if (skip == 0)
+            skip = 1;
+    }
+
+    public void generateGraph(Canvas canvas){
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+        Bitmap bmp = Bitmap.createBitmap(getWidth(), getHeight(), conf);
+
+        /*for(int x = 0; x < getWidth(); x+= 10){
+            for(int y = 0; y < getHeight(); y+= 10){
+                for(int z = 0; z <= 10; z++)
+                    if (x+z < getWidth() && y+z < getHeight())
+                     bmp.setPixel(x+z, y+z, Color.BLACK);
+            }
+        }
+        Log.d("OK", "HERE" + getHeight());*/
+        //canvas.drawBitmap(bmp, new Matrix(), null);
+        drawGrid(canvas);
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        generateStep();
+        generateGraph(canvas);
+        drawXAxisText(canvas);
+        drawYAxisText(canvas);
     }
 }
--- app/src/main/java/ca/horatiu/slope_field_generator/MainActivity.java
+++ app/src/main/java/ca/horatiu/slope_field_generator/MainActivity.java
@@ -8,6 +8,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Graph renderScreen = new Graph(this);
+        setContentView(renderScreen);
+
+        //setContentView(R.layout.activity_main);
+
     }
 }
